f618f8a794a6874d4af5aa0e927f027f6e838052
jason cheng <jasoncheng06100@gmail.com>
2019-06-20 17:40:17 +0800

    adjust layout

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -2,31 +2,44 @@
 
 @section('content')
 
-
+    <div class="card">
+        <div class="card-header">
             <h1>Recently View Articles</h1>
-
+        </div>
             <ul>
                 @foreach($reView as $article)
-
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
                     <li>
                         <a href="{{ route('article.show', $article->id) }}">{{ $article->title }}</a>
                     </li>
+                            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                        </blockquote>
+                    </div>
                 @endforeach
 
             </ul>
+    </div>
 
-
+    <div class="card">
+        <div class="card-header">
             <h1>All Articles</h1>
+        </div>
 
             <ul>
                 @foreach($articles as $article)
-
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
                     <li>
                         <a href="{{ route('article.show', $article->id) }}">{{ $article->title }}</a>
                     </li>
+                            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                        </blockquote>
+                    </div>
                 @endforeach
 
             </ul>
+    </div>
 @endsection
 
 
--- resources/views/articles/rank.blade.php
+++ resources/views/articles/rank.blade.php
@@ -3,29 +3,45 @@
 
 @section('content')
 
+    <div class="card">
+        <div class="card-header">
             <h1>Most View Articles</h1>
+        </div>
 
             <ul>
                 @foreach($rank as $article)
-
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
                     <li>
                         <a href="{{ route('article.show', $article->id) }}">{{ $article->title }}</a>
                     </li>
+                            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                        </blockquote>
+                    </div>
                 @endforeach
 
             </ul>
+    </div>
 
+    <div class="card">
+        <div class="card-header">
             <h1>All Articles</h1>
+        </div>
 
             <ul>
                 @foreach($articles as $article)
-
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
                     <li>
                         <a href="{{ route('article.show', $article->id) }}">{{ $article->title }}</a>
                     </li>
+                            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                        </blockquote>
+                    </div>
                 @endforeach
 
             </ul>
+    </div>
 @endsection
 
 
